5f1719e8a52d3e937f999baae26014cac4c9d48b
Samuel Mendes <samuel.mendes@openservice.com.br>
2015-12-17 21:55:25 -0200

    Implementing a ArrayAdpter<String> on MainActivityFragment, plus adding ListView on fragment_main.xml to render the forecast (instance of TextView).

--- app/src/main/java/app/com/example/samuel/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/com/example/samuel/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,25 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String[] data = {
+                "Today - Sunny - 83/73",
+                "Tomorrow - Foggy - 70/46",
+                "Friday - Cloudy - 30/10",
+                "Saturday - Rainy - 64/51",
+                "Sunday - Foggy - 40/20",
+                "Monday - Sunny - 99/80"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                            getActivity(),
+                                            R.layout.list_item_forecast,
+                                            R.id.list_item_forecast_textview,
+                                            weekForecast);
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
