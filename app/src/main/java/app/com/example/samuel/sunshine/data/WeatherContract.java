a2607e2d1e6ea52d1f943c6bdee76755d27b542c
Samuel Mendes <samuel.mendes@openservice.com.br>
2015-12-25 11:14:14 -0200

    Correct column miss typing.

--- app/src/main/java/app/com/example/samuel/sunshine/data/WeatherContract.java
+++ app/src/main/java/app/com/example/samuel/sunshine/data/WeatherContract.java
@@ -29,7 +29,7 @@ public class WeatherContract {
 
         public static final String COLUMN_CITY_NAME = "city_name";
         public static final String COLUMN_COORD_LAT = "coord_lat";
-        public static final String COLUMN_COORD_LONG = "corrd_long";
+        public static final String COLUMN_COORD_LONG = "coord_long";
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
 
     }
