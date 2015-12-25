315a5c42d3d9682a892792801676e0a716f7fa90
Samuel Mendes <samuelmendes.dev@gmail.com>
2015-12-25 17:39:00 -0200

    Extend ContentProvider abstract class

--- app/src/main/java/app/com/example/samuel/sunshine/data/WeatherProvider.java
+++ app/src/main/java/app/com/example/samuel/sunshine/data/WeatherProvider.java
@@ -1,7 +1,50 @@
 package app.com.example.samuel.sunshine.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
 /**
  * Created by samuel on 25/12/15.
  */
-public class WeatherProvider {
+public class WeatherProvider extends ContentProvider {
+
+
+
+
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
