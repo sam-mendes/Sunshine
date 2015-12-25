0b9faafb52bda78b45f041cf14b74e6195bb485e
Samuel Mendes <samuelmendes.dev@gmail.com>
2015-12-25 17:35:55 -0200

    Create Weather provider class in order to build a ContentProvider for querying sunshine data

--- app/src/androidTest/java/app/com/example/samuel/sunshine/data/TestWeatherContract.java
+++ app/src/androidTest/java/app/com/example/samuel/sunshine/data/TestWeatherContract.java
@@ -1,7 +1,12 @@
 package app.com.example.samuel.sunshine.data;
 
+import android.test.AndroidTestCase;
+
 /**
  * Created by samuel on 25/12/15.
  */
-public class TestWeatherContract {
+public class TestWeatherContract extends AndroidTestCase {
+    private static final String LOG_TAG = TestWeatherContract.class.getSimpleName();
+
+
 }
